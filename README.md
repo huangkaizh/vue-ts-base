188bc38744fe16aa3e9cb797fde7290330c69b87
huangkaizh <huangkaizh@163.com>
2019-11-14 17:06:16 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,13 @@
+# vue-base
+
+webpack4+vue+typescript+less+eslint+vscode 脚手架
+vscode 需要安装 ESLint、Prettier、Vetur 等插件, 并且安装依赖（npm install）之后才能正常使用 Eslint 功能（保存、格式化等操作时自动修复 eslint 警告）
+
+#安装依赖
+npm install
+
+#开发调试
+npm run dev
+
+#打包
+npm run build
---
+++ src/pages/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <article class="test">
+    <p>{{ vue }}</p>
+  </article>
+</template>
+
+<script lang="ts">
+import { Component, Provide, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'App'
+})
+export default class extends Vue {
+  @Provide() vue = 'vue'
+}
+</script>
+
+<style scoped>
+.test > p {
+  background-color: aqua;
+}
+</style>
+
+<style lang="less" scoped>
+.test > p {
+  color: red;
+}
+</style>
